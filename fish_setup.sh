7abe8ee28ea9a5deb0bca794595f638c17781044
William Ye <33244107+w3ye@users.noreply.github.com>
2022-03-03 21:40:17 -0500

    Update fish_setup.sh

--- fish_setup.sh
+++ fish_setup.sh
@@ -7,8 +7,5 @@ fisher install joseluisq/gitnow@2.10.0
 echo "---------- Installing fisher/you-should-use ----------"
 fisher install paysonwallach/fish-you-should-use
 
-echo "---------- Installing Starship prompt ----------"
-sh -c "curl -fsSL https://starship.rs/install.sh"
-
 echo "Setting fish as default prompt"
 sudo chsh -s /usr/bin/fish codespace
